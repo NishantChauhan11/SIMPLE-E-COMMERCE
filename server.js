f8a97dd92d601c3323733bcb2c4550036ff18373
Nishant <26nishantchauhan@gmail.com>
2025-08-05 10:59:42 +0530

    Fixed MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./src/db/db");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+connectDB();
+
 app.get("/", (req, res) => {
-    res.send("App is running!");
+  res.send("App is running!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
--- src/db/db.js
+++ src/db/db.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect(process.env.MONGODB)
+    .connect(process.env.MONGODB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("MongoDB connection successful"))
-    .catch((err) => console.error("Connection error:", err));
+    .catch((err) => {
+      console.error("Connection error:", err);
+      process.exit(1);
+    });
 };
 
 module.exports = connect;
